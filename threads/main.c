25d05f495ca42bacd4b13d4fc8f06d05d1f5f3e4
Adam <abol@sas.upenn.edu>
2018-05-09 15:33:37 -0500

    first commit

---
+++ threads/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  threads
+//
+//  Created by Adam on 5/9/18.
+//  Copyright © 2018 Adam. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
