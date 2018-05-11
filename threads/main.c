2d4d3d8cd80d94c8cd5bc1b911a184065433bb86
Adam <abol@sas.upenn.edu>
2018-05-11 13:53:18 -0500

    add hash tests and full test suite

--- threads/main.c
+++ threads/main.c
@@ -130,18 +130,74 @@ void list_test(int runs, int thread_count)
 	printf("Average Runtime: %ld\n", avg);
 }
 
+//HASH TESTS
+void * hash_insert(void * hash)
+{
+	for (int i = 0; i < MAX; i++)
+	{
+		Hash_Insert((hash_handle)hash, &numbers[i], numbers[i]);
+	}
+	return NULL;
+}
+
+void hash_test(int runs, int thread_count, int num_buckets)
+{
+	long times[runs];
+	for (int r = 0; r < runs; r++)
+	{
+		long start = get_cur_time();
+		pthread_t threads[thread_count];
+		hash_handle hash = (hash_handle)malloc(sizeof(*hash));
+		Hash_Init(hash, num_buckets);
+		for (int t = 0; t < thread_count; t++)
+		{
+			pthread_create(&threads[t], NULL, hash_insert, (void *)hash);
+		}
+		for (int t = 0; t < thread_count; t++)
+		{
+			pthread_join(threads[t], NULL);
+		}
+		long end = get_cur_time();
+		times[r] = end - start;
+	}
+	long avg = average(times, runs);
+	printf("Hash Test: %d Trials, %d Threads %d buckets: ", runs, thread_count, num_buckets);
+	print_array(times, runs);
+	printf("Average Runtime: %ld\n", avg);
+}
+
 int main(int argc, const char * argv[])
 {
+	//fill in global var for threads
 	for (int i = 0; i < MAX; i++)
 	{
 		numbers[i] = i + 1;
 	}
 	srand((unsigned int)time(NULL));
-	counter_test(1, 4);
-	counter_test(1, 8);
-	counter_test(1, 12);
-	list_test(3, 4);
-	list_test(3, 8);
-	list_test(3, 12);
+	//counter test
+	counter_test(10, 4);
+	counter_test(10, 8);
+	counter_test(10, 12);
+	counter_test(10, 16);
+	counter_test(10, 20);
+	//list-insertion test
+	list_test(10, 4);
+	list_test(10, 8);
+	list_test(10, 12);
+	list_test(10, 14);
+	list_test(10, 16);
+	list_test(10, 20);
+	//hash-table insertion test
+	hash_test(10, 4, 1000);
+	hash_test(10, 8, 1000);
+	hash_test(10, 12, 1000);
+	hash_test(10, 16, 1000);
+	hash_test(10, 20, 1000);
+	//hash-table scaling test
+	hash_test(10, 16, 10);
+	hash_test(10, 16, 100);
+	hash_test(10, 16, 1000);
+	hash_test(10, 16, 10000);
+	hash_test(10, 16, 100000);
 	return 0;
 }
